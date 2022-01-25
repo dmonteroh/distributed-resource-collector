d5e7b709a1c6a6ea4b52870fb1f4ced4e951d4f4
Daniel Montero <dmontero93@gmail.com>
2022-01-26 00:32:17 +0200

    Timestamp
    added timestamp to heartbeat

--- internal/serverStats.go
+++ internal/serverStats.go
@@ -1,13 +1,23 @@
 package internal
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+)
 
 type DcrStats struct {
+	Timestamp  DcrTimestamp     `json:"timestamp"`
 	CPUStats   DrcCPUStats      `json:"cpuStats"`
 	MemStats   DrcMemStats      `json:"memStats"`
 	DiskStats  []DrcDiskStats   `json:"diskStats"`
 	ProcStats  DrcProcStats     `json:"procStats"`
 	DockerSats []DrcDockerStats `json:"dockerStats"`
+}
+
+type DcrTimestamp struct {
+	TimeLocal   time.Time `json:"timeLocal"`
+	TimeSeconds int64     `json:"timeSeconds"`
+	TimeNano    int64     `json:"timeNano"`
 }
 
 func (d DcrStats) String() string {
@@ -16,8 +26,15 @@ func (d DcrStats) String() string {
 }
 
 func GetServerStats() (dcrStats DcrStats) {
+	tmpTime := time.Now()
+	timestamp := DcrTimestamp{
+		TimeLocal:   tmpTime,
+		TimeSeconds: tmpTime.Unix(),
+		TimeNano:    tmpTime.UnixNano(),
+	}
 
 	return DcrStats{
+		Timestamp:  timestamp,
 		CPUStats:   GetCPUUsage(),
 		MemStats:   GetMemoryUsage(),
 		DiskStats:  GetDiskUsage(),
